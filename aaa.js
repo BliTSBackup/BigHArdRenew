07c22a4ce3fed1956c67f777b2a3230e04076075
Mad Developer <122183994+BliTSBackup@users.noreply.github.com>
2023-01-11 21:18:42 -0500

    Create aaa.js

---
+++ aaa.js
@@ -0,0 +1,21 @@
+REDEFINE.refresh = () => {
+    REDEFINE.initUtils();
+    REDEFINE.initMenuShrink();
+    REDEFINE.initModeToggle();
+    REDEFINE.initBackToTop();
+
+    if (REDEFINE.theme_config.local_search.enable === true) {
+      REDEFINE.initLocalSearch();
+    }
+
+    if (REDEFINE.theme_config.code_block.copy === true) {
+      REDEFINE.initCopyCode();
+    }
+
+    if (REDEFINE.theme_config.lazyload.enable === true) {
+      REDEFINE.initLazyLoad();
+    }
+  }
+
+  REDEFINE.printThemeInfo();
+  REDEFINE.refresh();
